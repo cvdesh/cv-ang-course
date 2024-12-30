5abb8abd0c6715867693e463c4272a6441200cba
Chaitanya Deshpande <chaitanya11d@gmail.com>
2024-12-30 18:03:07 +0530

    Converted all apps in Module Based

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,8 @@
 import { Component, EventEmitter, Output, inject, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NewTask } from '../task/task.model';
 import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-new-task',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
